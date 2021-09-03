2b8f9599ca49ef2253073d7538e954b7c01d3cce
submit54 <blue_tissues@naver.com>
2021-09-03 16:32:15 +0900

    commit

--- js/background.js
+++ js/background.js
@@ -5,5 +5,5 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 const bgImage = document.createElement("img");
 bgImage.src = `img/${chosenImage}`;
 
-document.body.appendChild(bgImage);
-//prepend 가장 앞에 추가
+//document.body.appendChild(bgImage);
+document.body.style.backgroundImage = `url(img/${chosenImage})`;
--- js/greetings.js
+++ js/greetings.js
@@ -10,6 +10,8 @@ const greeting = document.querySelector("#greeting");
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
+const logoutBtn = document.querySelector("#logoutBtn");
+
 function onLoginSubmit(event) {
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
@@ -32,14 +34,29 @@ function onLoginBtnClick() {
 
 //loginButton.addEventListener("click", onLoginBtnClick);
 
+logoutBtn.addEventListener("click", onLogoutBtnClick);
+
 function paintGreetings(username) {
     greeting.innerText = `Hello  ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
+    //logoutBtn.classList.remove(HIDDEN_CLASSNAME);
+    logoutBtn.style.display = "";
 }
+
+function onLogoutBtnClick() {
+    localStorage.removeItem(USERNAME_KEY);
+    greeting.classList.add(HIDDEN_CLASSNAME);
+    logoutBtn.classList.add(HIDDEN_CLASSNAME);
+    logoutBtn.style.display = "none";
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginInput.value = "";
+}
+
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 if (savedUsername === null) {
     loginForm.classList.remove(HIDDEN_CLASSNAME);
+    logoutBtn.style.display = "none";
     loginForm.addEventListener("submit", onLoginSubmit);
 } else {
     paintGreetings(savedUsername);
